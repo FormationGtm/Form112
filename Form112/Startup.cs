24ccfb3ca3019bb1141a032216769468fadf5a0d
Daniel Lucazeau <contact@dlucazeau.fr>
2015-08-21 08:59:39 +0200

    Initialisation de la solution

---
+++ Form112/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Form112.Startup))]
+namespace Form112
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
