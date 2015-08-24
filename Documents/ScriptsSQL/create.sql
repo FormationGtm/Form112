a6ac6f3f4c231186ae84f17baee988a4a34fd835
adrycss <adrycss@icloud.com>
2015-08-24 15:28:04 +0200

    Création de la base et EntityFramework

--- Documents/ScriptsSQL/create.sql
+++ Documents/ScriptsSQL/create.sql
@@ -1,1 +1,78 @@
--- Création BDD
+use Form112
+
+CREATE TABLE Durees ( 
+	IdDuree              int NOT NULL   IDENTITY,
+	NbJours              int NOT NULL   ,
+	CONSTRAINT Pk_Durees PRIMARY KEY ( IdDuree )
+ );
+
+CREATE TABLE Promos ( 
+	IdPromo              int NOT NULL   IDENTITY,
+	Reduction            int NOT NULL   ,
+	CONSTRAINT Pk_Promos PRIMARY KEY ( IdPromo )
+ );
+
+CREATE TABLE Regions ( 
+	IdRegion             int NOT NULL   IDENTITY,
+	Nom                  nvarchar(30) NOT NULL   ,
+	CONSTRAINT Pk_Regions PRIMARY KEY ( IdRegion )
+ );
+
+CREATE TABLE Themes ( 
+	IdTheme              int NOT NULL   IDENTITY,
+	Nom                  nvarchar(50) NOT NULL   ,
+	CONSTRAINT Pk_Themes PRIMARY KEY ( IdTheme )
+ );
+
+CREATE TABLE Pays ( 
+	IdPays               char(3) NOT NULL   ,
+	IdRegion             int NOT NULL   ,
+	Nom                  nvarchar(30) NOT NULL   ,
+	CONSTRAINT Pk_Pays PRIMARY KEY ( IdPays ),
+	CONSTRAINT Pk_Pays_0 UNIQUE ( IdRegion ) 
+ );
+
+CREATE TABLE Ports ( 
+	IdPort               int NOT NULL   IDENTITY,
+	IdPays               char(3) NOT NULL   ,
+	Nom                  nvarchar(40) NOT NULL   ,
+	CONSTRAINT Pk_Ports PRIMARY KEY ( IdPort )
+ );
+
+CREATE INDEX idx_Ports ON Ports ( IdPays );
+
+CREATE TABLE Croisieres ( 
+	IdCroisiere          int NOT NULL   IDENTITY,
+	IdTheme              int NOT NULL   ,
+	IdDuree              int NOT NULL   ,
+	IdPromo              int NOT NULL   ,
+	IdPort               int NOT NULL   ,
+	Prix                 int NOT NULL   ,
+	DateDepart           date NOT NULL   ,
+	Photo                nvarchar(50) NOT NULL   ,
+	CONSTRAINT Pk_Croisieres PRIMARY KEY ( IdCroisiere )
+ );
+
+CREATE TABLE Photos ( 
+	IdPhoto              int NOT NULL   IDENTITY,
+	IdCroisiere          int NOT NULL   ,
+	NomPhoto             nvarchar(40) NOT NULL   ,
+	CONSTRAINT Pk_Photos PRIMARY KEY ( IdPhoto )
+ );
+
+CREATE INDEX idx_Photos ON Photos ( IdCroisiere );
+
+ALTER TABLE Croisieres ADD CONSTRAINT fk_croisieres_themes FOREIGN KEY ( IdTheme ) REFERENCES Themes( IdTheme ) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+ALTER TABLE Croisieres ADD CONSTRAINT fk_croisieres_durees FOREIGN KEY ( IdDuree ) REFERENCES Durees( IdDuree ) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+ALTER TABLE Croisieres ADD CONSTRAINT fk_croisieres_promos FOREIGN KEY ( IdPromo ) REFERENCES Promos( IdPromo ) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+ALTER TABLE Croisieres ADD CONSTRAINT fk_croisieres_ports FOREIGN KEY ( IdPort ) REFERENCES Ports( IdPort ) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+ALTER TABLE Pays ADD CONSTRAINT fk_pays_pays FOREIGN KEY ( IdRegion ) REFERENCES Regions( IdRegion ) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+ALTER TABLE Photos ADD CONSTRAINT fk_photos_croisieres FOREIGN KEY ( IdCroisiere ) REFERENCES Croisieres( IdCroisiere ) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+ALTER TABLE Ports ADD CONSTRAINT fk_ports_pays FOREIGN KEY ( IdPays ) REFERENCES Pays( IdPays ) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
