8c58a96bc6344847841402db0c036e4989411c88
crakotomalala <christine.rakotomalala@gmail.com>
2015-08-25 17:34:22 +0200

    màj

--- Form112/Infrastructure/Filters/NlogFilterAttribute.cs
+++ Form112/Infrastructure/Filters/NlogFilterAttribute.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using DataLayer.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
+using System.Web;
 
 
 namespace Form112.Infrastructure.Filters
@@ -10,14 +12,14 @@ namespace Form112.Infrastructure.Filters
     {
         public override void OnActionExecuting(ActionExecutingContext filterContext)
         {
-            var db = new OpenSeaEntities();
-            var la = new LogAction
-            {
-                DateNlog = DateTime.Now,
-                Action = filterContext.ActionDescriptor.ActionName,
-                Controller = filterContext.ActionDescriptor.ControllerDescriptor.ControllerName,                
-            };
-            db.LogAction.Add(la);
+            Form112Entities db = new Form112Entities();
+            //var la = new LogAction
+            //{
+            //    DateNlog = DateTime.Now,
+            //    Action = filterContext.ActionDescriptor.ActionName,
+            //    Controller = filterContext.ActionDescriptor.ControllerDescriptor.ControllerName,                
+            //};
+            //db.LogAction.Add(la);
             db.SaveChanges();
         }
     }
