88e0eaa8b099d99e5f816ce136b37b3cd08875a9
adrycss <adrycss@icloud.com>
2015-08-26 17:07:02 +0200

    Modifs BreadCrumb

---
+++ Form112/Infrastructure/BreadCrumbItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Form112.Infrastructure
+{
+    public class BreadCrumbItem
+    {
+        public string Texte { get; set; }
+
+        public string Route { get; set; }
+
+        public BreadCrumbItem (string texte, string route)
+        {
+            Texte = texte;
+            Route = route;
+        }
+    }
+}
