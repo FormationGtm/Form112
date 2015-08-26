83d8fe8de526c62bd5366c39aaeb0508d553d3cc
achollet <AlexisChollet@gmail.com>
2015-08-26 12:20:44 +0200

    Ajout de l'authentification

--- DataLayer/Model/OpenSea.Context.cs
+++ DataLayer/Model/OpenSea.Context.cs
@@ -34,5 +34,10 @@ namespace DataLayer.Model
         public virtual DbSet<Promos> Promos { get; set; }
         public virtual DbSet<Regions> Regions { get; set; }
         public virtual DbSet<Themes> Themes { get; set; }
+        public virtual DbSet<C__MigrationHistory> C__MigrationHistory { get; set; }
+        public virtual DbSet<AspNetRoles> AspNetRoles { get; set; }
+        public virtual DbSet<AspNetUserClaims> AspNetUserClaims { get; set; }
+        public virtual DbSet<AspNetUserLogins> AspNetUserLogins { get; set; }
+        public virtual DbSet<AspNetUsers> AspNetUsers { get; set; }
     }
 }
