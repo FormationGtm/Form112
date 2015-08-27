237392b630c0fb996fb81e851f90347b92ff88b2
achollet <AlexisChollet@gmail.com>
2015-08-27 14:55:04 +0200

    MàJ vue Pays

--- Form112/Controllers/HomeController.cs
+++ Form112/Controllers/HomeController.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using DataLayer.Model;
+using Form112.Infrastructure.SearchCroisiers.Option;
+using Form112.Infrastructure.SearchCroisiers.Base;
+using Form112.Infrastructure.SearchCroisiers;
+using Form112.Models;
 
 namespace Form112.Controllers
 {
@@ -27,6 +31,25 @@ namespace Form112.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+        private static List<Croisieres> GetPaysResult(HomeViewModels homeViewModel)
+        {
+            SearchBase search = new Search();
+            search = new SearchOptionPays(search, homeViewModel.PaysChoice);
+            return search.GetResult().ToList();
+        }
+
+        [HttpPost]
+        public ActionResult Pays(HomeViewModels hvm)
+        {
+            return View(GetPaysResult(hvm));
+        }
+
+        [ChildActionOnly]
+        public PartialViewResult TopSixCroisieres(int idCroisiere)
+        {
+            var croisiere = db.Croisieres.Find(idCroisiere);
+            return PartialView("_DestinationPanel", croisiere);
         }
 
         
