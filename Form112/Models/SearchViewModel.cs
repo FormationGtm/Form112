7368fef1c06e18b31a732ad50d3b5f1391af5514
achollet <AlexisChollet@gmail.com>
2015-08-28 14:05:05 +0200

    Merge

--- Form112/Models/SearchViewModel.cs
+++ Form112/Models/SearchViewModel.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using DataLayer.Model;
+using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Xml.Serialization;
@@ -9,10 +11,18 @@ namespace Form112.Models
 {
     public class SearchViewModel
     {
+        [XmlIgnore]
+        public List<Croisieres> Result { get; set; }
+        
+        [XmlIgnore]
         public Dictionary<string, Dictionary<string, string>> Destination { get; set; }
+
         public string IdPays { get; set; }
 
+
         public DateTime _dateDepart;
+
+        public string _stringDateDepart { get; set; }
 
         public string DateDepart
         {
@@ -24,21 +34,51 @@ namespace Form112.Models
                     DateTimeStyles.None, out _dateDepart))
                 {
                     _dateDepart = DateTime.Now;
+                }else
+                {
+                    _stringDateDepart = value;
                 }
             }
         }
 
         public int IdPortDepart { get; set; }
-        public int PrixMini { get; set; }
-        public int PrixMaxi { get; set; }
-        public int DureeMini { get; set; }
-        public int DureeMaxi { get; set; }
+
+        public int IdPrix { get; set; }
+
+        public int IdDuree { get; set; }
+
+
+        [XmlIgnore]
+        public List<KeyValuePair<int, string>> ListTranchePrix { get; set; }
+
+
+        [XmlIgnore]
+        public List<KeyValuePair<int, string>> ListTrancheDuree { get; set; }
+
+
+        [XmlIgnore]
         public List<KeyValuePair<int, string>> Themes { get; set; }
+
+        [XmlIgnore]
+        public List<KeyValuePair<int, string>> Ports { get; set; }
+
         public int IdTheme { get; set; }
 
+        public string XmlSearchViewModel { get; set; }
 
+        public void SerializeSearchViewModel()
+        {
+            var xmlSerializer = new XmlSerializer(typeof(SearchViewModel));
+            var writer = new StringWriter();
+            xmlSerializer.Serialize(writer, this);
+            XmlSearchViewModel = writer.ToString();
+        }
 
-
-		
+        public static SearchViewModel UnserializeSearchViewModel(string rvms)
+        {
+            var xmlSerializer = new XmlSerializer(typeof(SearchViewModel));
+            var reader = new StringReader(rvms);
+            return (SearchViewModel)xmlSerializer.Deserialize(reader);
+        }
     }
 }
