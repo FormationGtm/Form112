8f224bbdb64a30ec828cfeb0199a5bd8ea15832f
achollet <AlexisChollet@gmail.com>
2015-08-28 14:14:18 +0200

    Mise à Jour 28 aout

--- Form112/Scripts/Home/Index.js
+++ Form112/Scripts/Home/Index.js
@@ -1,19 +1,19 @@
 ﻿$(function () {
     $('#idVmapWorld').vectorMap({
-        map: 'world_en',
-        backgroundColor: '#a5bfdd',
-        borderColor: 'none',
-        borderOpacity: 0.25,
-        borderWidth: 1,
-        color: '#f4f3f0',
-        enableZoom: true,
-        hoverColor: '#c9dfaf',
-        hoverOpacity: null,
-        normalizeFunction: 'linear',
-        scaleColors: ['#b6d6ff', '#005ace'],
-        selectedColor: '#c9dfaf',
-        selectedRegion: null,
-        showTooltip: true,
+    map: 'world_en',
+    backgroundColor: '#a5bfdd',
+    borderColor: '#818181',
+    borderOpacity: 0.25,
+    borderWidth: 1,
+    color: '#f4f3f0',
+    enableZoom: true,
+    hoverColor: '#c9dfaf',
+    hoverOpacity: null,
+    normalizeFunction: 'linear',
+    scaleColors: ['#b6d6ff', '#005ace'],
+    selectedColor: '#c9dfaf',
+    selectedRegion: null,
+    showTooltip: true,
         onRegionClick: function (element, code, region) {
             $("#idPaysChoice").val(code.toUpperCase());
             $("#idFormMap").submit();
